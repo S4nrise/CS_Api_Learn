21a8fecce1c3d7626381fc176bd8ba27bf226e6e
S4nrise <kodzzekage@gmail.com>
2024-03-12 21:23:32 +0500

    1-7 hotfix

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -28,9 +28,10 @@ namespace Notes.WebApi
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<StartupBase>();
-            });
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
